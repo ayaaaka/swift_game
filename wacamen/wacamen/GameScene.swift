dc2b249c515b3625b6f838224d15f2d2ab950bba
Misaki0718 <misaaaki0718@gmail.com>
2017-12-29 23:45:12 +0900

    キャラクターの追加

--- wacamen/wacamen/GameScene.swift
+++ wacamen/wacamen/GameScene.swift
@@ -11,10 +11,52 @@ import GameplayKit
 
 class GameScene: SKScene {
     
-    private var label : SKLabelNode?
-    private var spinnyNode : SKShapeNode?
-    
-    override func didMove(to view: SKView) {
-
+    struct Constants{
+        static let playerImages = ["standing2","walking","walking2"]
     }
+    var player: SKSpriteNode!
+
+    override func didMove(to view: SKView) {
+        
+        
+        self.setunpBackground()
+        self.setupPlayer()
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        
+    }
+    
+    override func update(_ currentTime: TimeInterval) {
+        
+    }
+    
+    //背景画像を読み込む
+    func setunpBackground(){
+        let backgroundTexture = SKSpriteNode(texture: SKTexture(imageNamed: "background"))
+        addChild(backgroundTexture)
+        backgroundTexture.size = CGSize(width: self.frame.size.width * 2, height: self.frame.size.height * 2)
+    }
+    
+    func setupPlayer(){
+        var playerTexture = [SKTexture]()
+        
+        for i in Constants.playerImages{
+            let texture = SKTexture(imageNamed: i)
+            texture.filteringMode = .linear
+            playerTexture.append(texture)
+        }
+        
+        let playerAnimation = SKAction.animate(with: playerTexture, timePerFrame: 0.2)
+        let loopAnimation = SKAction.repeatForever(playerAnimation)
+        
+        player = SKSpriteNode(texture: playerTexture[0])
+        player.size = CGSize(width: player.size.width / 4, height: player.size.height / 4)
+        player.position = CGPoint(x: self.frame.size.width / 2, y: self.frame.size.height / 2)
+        player.run(loopAnimation)
+        
+        self.addChild(player)
+    }
+    
+ 
 }
