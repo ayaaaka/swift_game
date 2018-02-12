3b29face6e4c36057ecd2575c4e94ef19187aa42
ayakaK <user.email ayaka4132teddybear@gmail.com>
2018-02-12 21:14:44 +0900

    スタート画面と終了画面の作成

---
+++ wacamen/wacamen/ViewControllers/StartViewController.swift
@@ -0,0 +1,20 @@
+//
+//  StartViewController.swift
+//  wacamen
+//
+//  Created by 小林彩花 on 2018/02/12.
+//  Copyright © 2018年 小林彩花. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class StartViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+}
